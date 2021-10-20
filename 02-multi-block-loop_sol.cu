0a60cc70a45f23cff5ce3a001f862e256f0256da
N0toriou5 <49403790+N0toriou5@users.noreply.github.com>
2021-10-20 12:20:43 +0200

    Add files via upload

---
+++ 02-multi-block-loop_sol.cu
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+ * Refactor `loop` to be a CUDA Kernel. The new kernel should
+ * only do the work of 1 iteration of the original loop.
+ */
+
+__global__ void loop()
+{
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  printf("This is iteration number %d\n", i);
+  
+}
+
+int main()
+{
+  /*
+   * When refactoring `loop` to launch as a kernel, be sure
+   * to use the execution configuration to control how many
+   * "iterations" to perform.
+   *
+   * For this exercise, be sure to use more than 1 block in
+   * the execution configuration.
+   */
+   
+  loop<<<2, 5>>>();
+  cudaDeviceSynchronize();
+}
